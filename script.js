a269eaedd96758d17e48f9a2609283f07edbd294
JvBo2573 <152930003+JvBo2573@users.noreply.github.com>
2025-07-03 12:06:09 -0300

    aula 03/07
    criação do conversor no javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+function converter(){
+    const valor = parseFloat(document.getElementById("valor").value);
+    const moeda = document.getElementById('moeda').value;
+
+    console.log('teste');
+    fetch(`https://open.er-api.com/v6/latest/${moeda}`)
+    .then(
+        function(resposta){
+            return resposta.json();
+        }
+    ).then(
+        function(resposta){
+            let resultado = valor / resposta.rates.BRL;
+            document.getElementById('resultado').innerText = resultado;
+            console.log(resultado);
+        }
+        
+    ).catch();
+    
+}
